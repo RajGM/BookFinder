a3e8e2772fd1007cf34688c4a820e60f1c4c582f
RajGM <rajgmsocial19@gmail.com>
2021-07-11 20:28:56 +0530

    data rendering on frontEnd completed

--- public/javaScript/indexpage.js
+++ public/javaScript/indexpage.js
@@ -1,9 +1,19 @@
 console.log("Index JS is loading correctly");
 
+let myTable = document.getElementById("myTable");
+let row1 = document.getElementById("row1");
+
+let dataArray = [];
+let totalRowBuild = 0;
+let maxRow; 
+
 axios.get('/getData')
     .then(function (response) {
         // handle success
+        dataArray = response.data;
+        maxRow = Object.keys(dataArray).length;
         console.log(response.data);
+        fillTable();
     })
     .catch(function (error) {
         // handle error
@@ -12,3 +22,22 @@ axios.get('/getData')
     .then(function () {
         // always executed
     });
+
+function fillTable(){
+    for(let i=1;i<=maxRow;i++){
+        let currRow = myTable.insertRow(totalRowBuild + 1);
+        for(let j=0;j<3;j++){
+            let currCell = currRow.insertCell(j);
+            if(j==0){
+                currCell.innerHTML = dataArray[i-1]["title"];
+            }else if(j==1){
+                currCell.innerHTML = dataArray[i-1]["author"];
+            }else if(j==2){
+                currCell.innerHTML = dataArray[i-1]["publishedDate"];
+            } 
+        }
+        totalRowBuild++;
+
+    }
+}
+    
