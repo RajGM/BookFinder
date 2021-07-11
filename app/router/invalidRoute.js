999089588344f93d1a47cad8cfd26d585c5d583a
RajGM <rajgmsocial19@gmail.com>
2021-07-12 03:54:49 +0530

    deploy favicon test

--- app/router/invalidRoute.js
+++ app/router/invalidRoute.js
@@ -5,7 +5,7 @@ var path = require('path');
 
 // @type    GET
 //@route    Any invalid route
-// @desc    for sending error page
+// @desc    for sending to home page
 // @access  PUBLIC
 router.get('/',function(req,res){
     res.sendFile(path.join(__dirname+'./../views/'+'index.html'));
@@ -13,7 +13,7 @@ router.get('/',function(req,res){
 
 // @type    POST
 //@route    Any invalid route
-// @desc    for sending error page
+// @desc    for sending to home page
 // @access  PUBLIC
 router.post('/',function(req,res){
     res.sendFile(path.join(__dirname+'./../views/'+'index.html'));
