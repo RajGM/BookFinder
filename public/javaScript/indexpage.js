83fbdb0f0078a3a419865105f3a6bc700bdbda9e
RajGM <rajgmsocial19@gmail.com>
2021-07-11 17:27:09 +0530

    getting all book data on client side using get request completed successfully

--- public/javaScript/indexpage.js
+++ public/javaScript/indexpage.js
@@ -1,1 +1,14 @@
 console.log("Index JS is loading correctly");
+
+axios.get('/getData')
+    .then(function (response) {
+        // handle success
+        console.log(response.data);
+    })
+    .catch(function (error) {
+        // handle error
+        console.log(error);
+    })
+    .then(function () {
+        // always executed
+    });
